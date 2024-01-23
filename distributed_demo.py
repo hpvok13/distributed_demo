95f7ac986114e8d15f7fb9c66560e2c42c9b3dbf
Valmiki Kothare <valmiki@mit.edu>
2024-01-23 15:47:42 -0500

    Updated Fabric code and config file

--- distributed_demo.py
+++ distributed_demo.py
@@ -9,14 +9,23 @@ from torch.utils.data import random_split
 from tqdm import tqdm
 
 
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
 # Define the model
 class SmallResNet(nn.Module):
-    def __init__(self, num_classes=10):
+    def __init__(self, image_shape, num_classes, **kwargs):
         super(SmallResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        channels, height, width = image_shape
+        self.conv1 = nn.Conv2d(
+            channels, 16, kernel_size=3, stride=1, padding=1
+        )
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
-        self.fc = nn.Linear(16 * 32 * 32, num_classes)
+        self.fc = nn.Linear(16 * height * width, num_classes)
 
     def forward(self, x):
         out = self.conv1(x)
@@ -28,45 +37,58 @@ class SmallResNet(nn.Module):
         return out
 
 
-def get_dataloaders():
+def get_dataloaders(data_dir, batch_size):
     transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
     )
 
     train_set = torchvision.datasets.CIFAR10(
-        root="./data", train=True, download=True, transform=transform
+        root=data_dir, train=True, download=True, transform=transform
     )
     test_set = torchvision.datasets.CIFAR10(
-        root="./data", train=False, download=True, transform=transform
+        root=data_dir, train=False, download=True, transform=transform
     )
 
     train_loader = torch.utils.data.DataLoader(
-        train_set, batch_size=128, shuffle=True, num_workers=0
+        train_set, batch_size=batch_size, shuffle=True, num_workers=0
     )
     test_loader = torch.utils.data.DataLoader(
-        test_set, batch_size=128, shuffle=False, num_workers=0
+        test_set, batch_size=batch_size, shuffle=False, num_workers=0
     )
 
     return train_loader, test_loader
 
 
 def train(config):
-    torch.manual_seed(10)
-    fabric = L.Fabric(accelerator="auto", devices="auto", num_nodes=2, strategy="auto")
+    fabric = L.Fabric(
+        accelerator=config.accelerator,
+        devices=config.devices,
+        strategy=config.strategy,
+        num_nodes=config.num_nodes,
+    )
+    fabric.launch()
 
+    torch.manual_seed(10)
     # Initialize the model and optimizer
-    model = SmallResNet()
+    model = SmallResNet(**config)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.AdamW(model.parameters(), lr=config.lr)
 
     # Get the dataloaders
-    train_loader, test_loader = get_dataloaders()
+    train_loader, test_loader = get_dataloaders(
+        config.data_dir, config.batch_size
+    )
 
     model, optimizer = fabric.setup(model, optimizer)
-    train_loader, test_loader = fabric.setup_dataloaders(train_loader, test_loader)
+    train_loader, test_loader = fabric.setup_dataloaders(
+        train_loader, test_loader
+    )
 
     # Training loop
-    for epoch in range(10):
+    for epoch in range(config.epochs):
         running_loss = 0.0
         running_correct = 0
         running_total = 0
@@ -117,10 +139,13 @@ def train(config):
 
         acc = 100 * test_correct / test_total
         test_loss /= len(test_loader)
-        fabric.print(f"Test Set Results | Loss: {test_loss:.4f} | Acc: {acc:.2%}")
+        fabric.print(
+            f"Test Set Results | Loss: {test_loss:.4f} | Acc: {acc:.2%}"
+        )
 
 
 if __name__ == "__main__":
     with open("config.yaml", "r") as f:
         config = yaml.safe_load(f)
+    config = AttrDict(config)
     train(config)
