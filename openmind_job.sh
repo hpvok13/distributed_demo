6853c9c2b4dbbdb029f7871c0cb9ffd52be69a4d
Valmiki Kothare <valmiki@mit.edu>
2024-01-23 16:10:24 -0500

    Modified job.sh

--- openmind_job.sh
+++ openmind_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH -J dist_job
+#SBATCH -J dist_demo_job
 #SBATCH -t 08:00:00
 #SBATCH --gres=gpu:2
 #SBATCH -N 1
 #SBATCH -c 1
 #SBATCH --constraint=rocky8
 #SBATCH --mem 10G
-#SBATCH -o dist_job.out
+#SBATCH -o job.out
 
-source /etc/profile
-module load anaconda/Python-ML-2023b
+source ~/.bashrc
+conda activate pytorch-3.10
 
 python distributed_demo.py
